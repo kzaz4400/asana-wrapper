746bd7b842c6892bd1d6325803e5c7696a2242c2
kzaz4400 <kazuhiko.azezaki@gmail.com>
2024-06-11 10:10:04 +0900

    docs/整理

--- src/AsanaWrapper/errors/ConnectionException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace kzaz4400\AsanaWrapper\errors;
-
-use Exception;
-
-class ConnectionException extends Exception
-{
-}
--- src/AsanaWrapper/errors/NotVerificationException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace kzaz4400\AsanaWrapper\errors;
-
-use Exception;
-
-class NotVerificationException extends Exception
-{
-}
